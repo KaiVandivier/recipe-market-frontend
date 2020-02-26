8d2d6e68fbd8f63d7413287cc252fa35fa66438e
Kai Vandivier <kpvandivier@gmail.com>
2020-02-25 20:06:28 -0800

    New styles and add link to test nprogress

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Home = () => (
   <div>
-    <h1>Home</h1>
+    <h2>Home</h2>
     <Link href="/items">
       <a>Go to items</a>
     </Link>
--- pages/items.js
+++ pages/items.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from "next/link";
 
 const ItemsPage = () => {
   return (
     <div>
+      <h2>Items</h2>
       <p>I'm the items page!</p>
+      <Link href="/">
+        <a>Go to home</a>
+      </Link>
     </div>
   );
 };
