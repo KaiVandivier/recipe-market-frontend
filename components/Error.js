7ddf93a7a7850c1160655c8aebd7a125fc4a7c18
Kai Vandivier <kpvandivier@gmail.com>
2020-03-08 18:48:09 -0700

    Update error message

--- components/Error.js
+++ components/Error.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const Error = ({ error }) => {
-  return (
+  if (!error || !error.message) return null;
+  return error ? (
     <div>
-      <p><em>Oops! Something went wrong. Here's the error message: </em></p>
-      <p>"{error.message}"</p>
+      <p>
+        <em>Oops! Something went wrong. Here's the error message: </em>
+      </p>
+      <p>"{error.message.replace("GraphQL error: ", "")}"</p>
     </div>
-  );
+  ) : null;
 };
 
 export default Error;
