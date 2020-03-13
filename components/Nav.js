4df4f1fd45b63ea68a236c99409fc46e209bcadc
Kai Vandivier <kpvandivier@gmail.com>
2020-03-12 19:09:46 -0700

    Add toggle cart mutation

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import { Mutation } from "@apollo/react-components";
 import User from "./User";
 import Signout from "./Signout";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 
 const StyledNav = styled.nav`
   display: flex;
@@ -15,45 +17,50 @@ const StyledNav = styled.nav`
 
 const Nav = () => {
   return (
-    <User>
-      {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <Error error={error} />;
-        return (
-          <StyledNav>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-            <Link href="/recipes">
-              <a>Recipes</a>
-            </Link>
-            <Link href="/items">
-              <a>Items</a>
-            </Link>
-            {data.currentUser ? (
-              <>
-                <Link href="/createItem">
-                  <a>Create Item</a>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => (
+        <User>
+          {({ data, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <Error error={error} />;
+            return (
+              <StyledNav>
+                <Link href="/">
+                  <a>Home</a>
                 </Link>
-                <Link href="/cart">
-                  <a>Cart</a>
+                <Link href="/recipes">
+                  <a>Recipes</a>
                 </Link>
-                <Signout />
-              </>
-            ) : (
-              <>
-                <Link href="/signin">
-                  <a>Sign In</a>
+                <Link href="/items">
+                  <a>Items</a>
                 </Link>
-                <Link href="/signup">
-                  <a>Sign Up</a>
-                </Link>
-              </>
-            )}
-          </StyledNav>
-        );
-      }}
-    </User>
+                {data.currentUser ? (
+                  <>
+                    <Link href="/createItem">
+                      <a>Create Item</a>
+                    </Link>
+                    {/* TODO: Also make a `cart` page */}
+                    <Link href="/cart">
+                      <a onClick={toggleCart}>Cart</a>
+                    </Link>
+                    <Signout />
+                  </>
+                ) : (
+                  <>
+                    <Link href="/signin">
+                      <a>Sign In</a>
+                    </Link>
+                    <Link href="/signup">
+                      <a>Sign Up</a>
+                    </Link>
+                  </>
+                )}
+              </StyledNav>
+            );
+          }}
+        </User>
+      )}
+    </Mutation>
   );
 };
 
