e63e45296fa626b2586aea58d08d7d195c2925b1
Kai Vandivier <kpvandivier@gmail.com>
2020-03-16 16:24:54 -0700

    Update `Error` to handle network errors

--- components/Error.js
+++ components/Error.js
@@ -2,14 +2,29 @@ import React from "react";
 
 const Error = ({ error }) => {
   if (!error || !error.message) return null;
-  return error ? (
+  console.log(error);
+  if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
+    return (
+      <div>
+        {error.networkError.result.errors.map((error, i) => (
+          <div key={i}>
+            <p>
+              <em>Oops! Something went wrong.</em>
+            </p>
+            <p>{JSON.stringify(error.message).replace("GraphQL error: ", "")}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
+  return (
     <div>
       <p>
         <em>Oops! Something went wrong.</em>
       </p>
-      <p>{error.message.replace("GraphQL error: ", "")}</p>
+      <p>{JSON.stringify(error.message).replace("GraphQL error: ", "")}</p>
     </div>
-  ) : null;
+  );
 };
 
 export default Error;
