90f3f4915b5c04cd4d1a0e28f9838262d6248273
Kai Vandivier <kpvandivier@gmail.com>
2020-03-16 18:29:03 -0700

    Add user (cart) refetch

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Mutation } from "@apollo/react-components";
 import { gql } from "apollo-boost";
+import { CURRENT_USER_QUERY } from "./User";
 
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($itemId: ID!, $quantity: Int = 1) {
@@ -15,7 +16,7 @@ const ADD_TO_CART_MUTATION = gql`
 
 const AddToCart = (props) => {
   return (
-    <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ ...props }}>
+    <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ ...props }} refetchQueries={[{ query: CURRENT_USER_QUERY }]}>
       {(addToCart, { loading, error, called }) => (
         <button onClick={addToCart}>Add To Cart</button>
       )}
