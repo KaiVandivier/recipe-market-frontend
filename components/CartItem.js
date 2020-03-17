11863f554e215c889361f0a7b93b4f5f816769c7
Kai Vandivier <kpvandivier@gmail.com>
2020-03-17 13:51:27 -0700

    Make cartItem component

---
+++ components/CartItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import RemoveFromCart from "./RemoveFromCart";
+import formatMoney from "../lib/formatMoney";
+
+const CartItemStyles = styled.li`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr 1fr;
+  .info {
+    padding: 0.5rem;
+  }
+  .subtotal {
+    padding: 0.5rem;
+  }
+  .error {
+    grid-column: 1 / span 3;
+  }
+`;
+
+const CartItem = ({ cartItem }) => {
+  return (
+    <CartItemStyles>
+      {/** TODO: subtotal */}
+      {cartItem.item ? (
+        <>
+          <img src={cartItem.item.image} width="100px" />
+          <div className="info">
+            <h3>{cartItem.item.title}</h3>
+            <p>
+              <em>
+                {cartItem.quantity} &times; {formatMoney(cartItem.item.price)}
+              </em>
+            </p>
+          </div>
+          <div className="subtotal">
+            <h3>{formatMoney(cartItem.quantity * cartItem.item.price)}</h3>
+          </div>
+        </>
+      ) : (
+        <div class="error">
+          <h3>This item has been deleted.</h3>
+        </div>
+      )}
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
+
+export default CartItem;
