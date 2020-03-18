9730fb37e870853b3915f92225e1fbc72b77a081
Kai Vandivier <kpvandivier@gmail.com>
2020-03-18 13:18:25 -0700

    Add createRecipe page

---
+++ pages/createRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateRecipe from "../components/CreateRecipe";
+
+const CreateRecipePage = () => {
+  return (
+    <div>
+      <CreateRecipe />
+    </div>
+  );
+};
+
+export default CreateRecipePage;
