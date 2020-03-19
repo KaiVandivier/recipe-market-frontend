a7e34ef1f27ba309bce95b09ce7d289c673c4fe6
Kai Vandivier <kpvandivier@gmail.com>
2020-03-19 16:39:02 -0700

    Move IngredientPicker outside main form

--- components/CreateRecipe.js
+++ components/CreateRecipe.js
@@ -4,6 +4,7 @@ import { gql } from "apollo-boost";
 import Form from "./styles/Form";
 import Error from "./Error";
 import PleaseSignIn from "./PleaseSignIn";
+import IngredientPicker from "./IngredientPicker";
 
 const CREATE_RECIPE_MUTATION = gql`
   mutation CREATE_RECIPE_MUTATION(
@@ -13,9 +14,9 @@ const CREATE_RECIPE_MUTATION = gql`
     $ingredients: [RecipeItemCreateInput!]!
   ) {
     createRecipe(
-      title: $title,
-      description: $description,
-      instructions: $instructions,
+      title: $title
+      description: $description
+      instructions: $instructions
       ingredients: $ingredients
     ) {
       id
@@ -29,14 +30,46 @@ class CreateRecipe extends Component {
     description: "",
     instructions: "",
     image: "",
-    items: [],
+    ingredients: [],
+    newIngredient: null,
+    quantity: ""
+  };
+
+  submitNewIngredient = () => {
+    console.log("submitting new");
+    const { newIngredient, quantity } = this.state;
+    const newIngredients = [
+      ...this.state.ingredients,
+      { item: newIngredient, quantity }
+    ];
+    this.setState({
+      ingredients: newIngredients
+    });
+  };
+
+  deleteIngredient = deleteId => {
+    console.log("deleting");
+    const newIngredients = [...this.state.ingredients].filter(
+      ingredient => ingredient.item.id !== deleteId
+    );
+    this.setState({
+      ingredients: newIngredients
+    });
+  };
+
+  setNewIngredientState = item => {
+    console.log("changing state");
+    this.setState({
+      newIngredient: item
+    });
   };
 
   handleChange = e => {
+    console.log("handling change");
     const { name, value, type } = e.target;
     this.setState({
-      [name]: (type === "number") ? Number(value) : value
-    })
+      [name]: type === "number" ? Number(value) : value
+    });
   };
 
   uploadFile = async e => {
@@ -45,24 +78,28 @@ class CreateRecipe extends Component {
     data.append("file", files[0]);
     data.append("upload_preset", "recipe-market-items");
 
-    const res = await fetch("https://api.cloudinary.com/v1_1/dliulozye/image/upload", { method: "post", body: data });
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dliulozye/image/upload",
+      { method: "post", body: data }
+    );
     const file = await res.json();
 
     this.setState({
       image: file.secure_url,
       largeImage: file.eager[0].secure_url
-    })
-  }
+    });
+  };
 
   clearForm = () => {
+    console.log("clearing");
     this.setState({
       title: "",
       description: "",
       instructions: "",
       image: "",
-      items: [],
-    })
-  }
+      ingredients: []
+    });
+  };
 
   render() {
     return (
@@ -71,66 +108,85 @@ class CreateRecipe extends Component {
           {(createRecipe, { loading, error, called }) => {
             if (error) return <Error error={error} />;
             return (
-              <Form onSubmit={(e) => {
-                e.preventDefault();
-                e.target.reset();
-                createRecipe();
-                this.clearForm();
-              }}>
+              <div>
                 <h1>Create a Recipe!</h1>
                 {!error && !loading && called && (
                   <h3>Recipe created successfully!</h3>
                 )}
-                <fieldset aria-disabled={loading}>
-                  <label htmlFor="title">
-                    Title:
-                    <input
-                      id="title"
-                      type="text"
-                      name="title"
-                      placeholder="Recipe Title"
-                      required
-                      value={this.state.title}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                  <label htmlFor="description">
-                    Description:
-                    <textarea
-                      id="description"
-                      name="description"
-                      placeholder="Recipe Description"
-                      value={this.state.description}
-                      onChange={this.handleChange}
-                    />
-                  </label>                  
-                  <label htmlFor="instructions">
-                    Instructions:
-                    <textarea
-                      id="instructions"
-                      name="instructions"
-                      placeholder="Recipe Instructions"
-                      value={this.state.instructions}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                  <label htmlFor="image">
-                    Image:
-                    <input
-                      id="image"
-                      type="file"
-                      name="image"
-                      placeholder="Upload an image"
-                      required
-                      // value={this.state.image}
-                      onChange={this.uploadFile}
-                    />
-                  </label>
-                  {/* TODO: Item search and add */}
-                  {this.state.image && <div><img src={this.state.image} /></div>}
-                  <button type="submit">Creat{loading ? "ing" : "e"} Recipe</button>
-                </fieldset>
-              </Form>
+                <IngredientPicker
+                  handleChange={this.handleChange}
+                  setNewIngredientState={this.setNewIngredientState}
+                  submitNewIngredient={this.submitNewIngredient}
+                  deleteIngredient={this.deleteIngredient}
+                  ingredients={this.state.ingredients}
+                  newIngredient={this.state.newIngredient}
+                  quantity={this.state.quantity}
+                />
+                <Form
+                  onSubmit={e => {
+                    console.log("submitting form");
+                    e.preventDefault();
+                    e.target.reset();
+                    createRecipe();
+                    // this.clearForm();
+                  }}
+                >
+                  <fieldset aria-disabled={loading}>
+                    <label htmlFor="title">
+                      Title:
+                      <input
+                        id="title"
+                        type="text"
+                        name="title"
+                        placeholder="Recipe Title"
+                        required
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description:
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Recipe Description"
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="instructions">
+                      Instructions:
+                      <textarea
+                        id="instructions"
+                        name="instructions"
+                        placeholder="Recipe Instructions"
+                        value={this.state.instructions}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="image">
+                      Image:
+                      <input
+                        id="image"
+                        type="file"
+                        name="image"
+                        placeholder="Upload an image"
+                        required
+                        // value={this.state.image}
+                        onChange={this.uploadFile}
+                      />
+                    </label>
+                    {this.state.image && (
+                      <div>
+                        <img src={this.state.image} />
+                      </div>
+                    )}
+                    <button type="submit">
+                      Creat{loading ? "ing" : "e"} Recipe
+                    </button>
+                  </fieldset>
+                </Form>
+              </div>
             );
           }}
         </Mutation>
