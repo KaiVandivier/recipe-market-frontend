da69ad5fabc75c16084d3ea267573378425d6d2f
Kai Vandivier <kpvandivier@gmail.com>
2020-03-20 14:43:08 -0700

    Refactor

--- components/styles/Card.js
+++ components/styles/Card.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import styled from "styled-components";
 
-const CardStyles = styled.div`
+const Card = styled.div`
   width: auto;
   padding: 2rem;
   background-color: white;
   box-shadow: 0 0 5px 3px rgba(0,0,0,0.1);
 `;
-
-const Card = ({ children }) => {
-  return (
-    <CardStyles>
-      {children}
-    </CardStyles>
-  );
-};
 
 export default Card;
