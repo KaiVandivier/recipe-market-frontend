6e0cb045cc59429d5d193a71d878a7e7f81625fa
Kai Vandivier <kpvandivier@gmail.com>
2020-04-02 11:10:32 -0700

    Make a button style

---
+++ components/styles/Button.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
+  font-size: 1rem;
+  font-weight: 800;
+  cursor: pointer;
+  color: ${props => {
+    if (props.primary || props.secondary) return "white";
+    return props.theme.darkgrey
+  }};
+  border: 3px solid ${props => (
+    props.primary ? props.theme.yellow : props.theme.darkgrey
+  )};
+  border-radius: 0.5rem;
+  background: ${props => {
+    if (props.primary) return props.theme.yellow;
+    if (props.secondary) return props.theme.black;
+    return props.theme.white;
+  }};
+`;
+
+export default Button;
