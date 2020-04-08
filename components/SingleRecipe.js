4475175de8c104f57a5611c4f8ec1fc2e5e6728c
Kai Vandivier <kpvandivier@gmail.com>
2020-04-07 21:32:22 -0700

    Add edit/delete buttons to page

--- components/SingleRecipe.js
+++ components/SingleRecipe.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Query } from "@apollo/react-components";
 import { gql } from "apollo-boost";
 import styled from "styled-components";
+import Link from "next/link";
 import Card from "./styles/Card";
+import Button from "./styles/Button";
 import Ingredient from "./Ingredient";
 import AddRecipeToCart from "./AddRecipeToCart";
+import DeleteRecipe from "./DeleteRecipe";
 
 const SINGLE_RECIPE_QUERY = gql`
   query SINGLE_RECIPE_QUERY($id: ID!) {
@@ -37,6 +40,9 @@ const SingleRecipeStyles = styled.section`
       box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.1);
     }
   }
+  .buttons {
+    text-align: center;
+  }
 `;
 
 const SingleRecipe = ({ id }) => {
@@ -62,7 +68,9 @@ const SingleRecipe = ({ id }) => {
 
                 {image ? <img src={image} alt={title} height="200px" /> : null}
 
-                <p><em>{description}</em></p>
+                <p>
+                  <em>{description}</em>
+                </p>
               </div>
 
               <h2>Ingredients:</h2>
@@ -75,6 +83,14 @@ const SingleRecipe = ({ id }) => {
 
               <h2>Instructions:</h2>
               <p>{instructions}</p>
+
+              <div className="buttons">
+                <AddRecipeToCart id={id} />
+                <Link href={{ pathname: "editRecipe", query: { id } }}>
+                  <Button>Edit Recipe</Button>
+                </Link>
+                <DeleteRecipe id={id} />
+              </div>
             </SingleRecipeStyles>
           </Card>
         );
