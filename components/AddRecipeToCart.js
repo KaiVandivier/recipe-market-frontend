98906b539d5f8e0cff25e18b918872de7bab33ba
Kai Vandivier <kpvandivier@gmail.com>
2020-04-10 15:36:02 -0700

    Add custom quantities

--- components/AddRecipeToCart.js
+++ components/AddRecipeToCart.js
@@ -1,41 +1,62 @@
-import React from "react";
-import { Mutation } from "@apollo/react-components";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
+import styled from "styled-components";
 import { CURRENT_USER_QUERY } from "./User";
 import Button from "./styles/Button";
 import Success from "./Success";
 
 const ADD_RECIPE_TO_CART_MUTATION = gql`
-  mutation ADD_RECIPE_TO_CART_MUTATION($id: ID!, $quantity: Int = 1) {
+  mutation ADD_RECIPE_TO_CART_MUTATION($id: ID!, $quantity: Float = 1) {
     addRecipeToCart(id: $id, quantity: $quantity) {
       id
     }
   }
 `;
 
-// TODO: Incorporate quantity, either in here or on the card/page
-// (I think in here is better)
+const QtyStyles = styled.input`
+  max-width: 4rem;
+`;
+
 const AddRecipeToCart = ({ id }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [addRecipeToCart, { loading, error, called }] = useMutation(
+    ADD_RECIPE_TO_CART_MUTATION,
+    {
+      variables: { id, quantity },
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
+
+  function handleChangeWrapper(setState) {
+    return function handleChange(e) {
+      const { value, type } = e.target;
+      setState(type === "number" ? Number(value) : value);
+    };
+  }
+
   return (
-    <Mutation
-      mutation={ADD_RECIPE_TO_CART_MUTATION}
-      variables={{ id }}
-      refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-    >
-      {(addRecipeToCart, { loading, error, called }) => {
-        if (loading) return <p>Loading...</p>;
-        return (
-          <>
-            {!error && !loading && called && (
-              <Success message={"Ingredients added to cart."} />
-            )}
-            <Button primary onClick={addRecipeToCart}>
-              Add Recipe Ingredients to Cart
-            </Button>
-          </>
-        );
-      }}
-    </Mutation>
+    <div>
+      {!error && !loading && called && (
+        <Success message={"Ingredients added to cart."} />
+      )}
+      <Button primary onClick={addRecipeToCart}>
+        Add Recipe Ingredients to Cart
+      </Button>
+      <label htmlFor={`quantity-${id}`}>
+        Qty:{" "}
+        <QtyStyles
+          id={`quantity-${id}`}
+          type="number"
+          name="quantity"
+          min="0"
+          step="any"
+          required
+          value={quantity}
+          onChange={handleChangeWrapper(setQuantity)}
+        />
+      </label>
+    </div>
   );
 };
 
--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,26 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
+import styled from "styled-components";
 import { CURRENT_USER_QUERY } from "./User";
 import Button from "./styles/Button";
 
 const ADD_TO_CART_MUTATION = gql`
-  mutation ADD_TO_CART_MUTATION($itemId: ID!, $quantity: Int = 1) {
+  mutation ADD_TO_CART_MUTATION($itemId: ID!, $quantity: Float = 1) {
     addToCart(itemId: $itemId, quantity: $quantity) {
       id
     }
   }
 `;
 
-const AddToCart = (props) => {
-  const [addToCart, { loading, error, called }] = useMutation(ADD_TO_CART_MUTATION, {
-    variables: { ...props },
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
-  })
+const QtyStyles = styled.input`
+  max-width: 4rem;
+`;
+
+const AddToCart = ({ id }) => {
   const [quantity, setQuantity] = useState(1);
+  const [addToCart, { loading, error, called }] = useMutation(
+    ADD_TO_CART_MUTATION,
+    {
+      variables: { id, quantity },
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
+
+  function handleChangeWrapper(setState) {
+    return function handleChange(e) {
+      const { value, type } = e.target;
+      setState(type === "number" ? Number(value) : value);
+    }
+  };
 
   return (
-        <Button primary onClick={addToCart}>Add To Cart</Button>
+    <div>
+      <Button primary onClick={addToCart}>
+        Add To Cart
+      </Button>
+      <label htmlFor={`quantity-${id}`}>
+        Qty:{" "}
+        <QtyStyles
+          id={`quantity-${id}`}
+          type="number"
+          name="quantity"
+          min="0"
+          step="any"
+          required
+          value={quantity}
+          onChange={handleChangeWrapper(setQuantity)}
+        />
+      </label>
+    </div>
   );
 };
 
