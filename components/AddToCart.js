0b67d6329d80af0da35d29648a4ad18c4cb982b0
Kai Vandivier <kpvandivier@gmail.com>
2020-04-10 13:06:25 -0700

    Refactor to use apollo hooks

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Mutation } from "@apollo/react-components";
+import React, { useState } from 'react';
+import { useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import { CURRENT_USER_QUERY } from "./User";
 import Button from "./styles/Button";
@@ -13,12 +13,14 @@ const ADD_TO_CART_MUTATION = gql`
 `;
 
 const AddToCart = (props) => {
+  const [addToCart, { loading, error, called }] = useMutation(ADD_TO_CART_MUTATION, {
+    variables: { ...props },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  })
+  const [quantity, setQuantity] = useState(1);
+
   return (
-    <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ ...props }} refetchQueries={[{ query: CURRENT_USER_QUERY }]}>
-      {(addToCart, { loading, error, called }) => (
         <Button primary onClick={addToCart}>Add To Cart</Button>
-      )}
-    </Mutation>
   );
 };
 
