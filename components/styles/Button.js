ea0c722390963ad4d8c406ec92a1427763c7f848
Kai Vandivier <kpvandivier@gmail.com>
2020-04-10 15:38:19 -0700

    Button loses shadow when pressed

--- components/styles/Button.js
+++ components/styles/Button.js
@@ -22,7 +22,7 @@ const Button = styled.button`
     return props.theme.white;
   }};
   transition: box-shadow 0.3s;
-  &:hover {
+  &:hover:not(:active) {
     box-shadow:
       0 2.8px 2.2px rgba(0, 0, 0, 0.034),
       0 6.7px 5.3px rgba(0, 0, 0, 0.048),
