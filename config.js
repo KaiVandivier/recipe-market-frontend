c8ca932976094fcbddc29ebe2b2c50b704b10244
Kai Vandivier <kpvandivier@gmail.com>
2020-04-13 13:49:01 -0700

    Add prod endpoint

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const endpoint = "http://localhost:4000";
-// TODO
-export const prodEndpoint = "TODO"
+export const prodEndpoint = "https://recipe-market-yoga.herokuapp.com"
 export const perPage = 4;
 export const stripePublicKey = "pk_test_F9Gx6bSEIkHo48NKN6YJJ1Nd00KpOwU7pE";
