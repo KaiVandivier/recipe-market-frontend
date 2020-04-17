000061ecfbb9e02a449b593803f85835a066a684
Kai Vandivier <kpvandivier@gmail.com>
2020-04-17 15:54:47 -0700

    Test a local state query

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import App from "next/app";
 import { ApolloProvider } from "@apollo/react-hooks";
 import withData from "../lib/withData";
 import Page from "../components/Page";
+import { gql } from "apollo-boost"; //
+import { Query } from "@apollo/react-components"; //
 import "../public/nprogress.css";
 
 class MyApp extends App {
@@ -12,19 +14,30 @@ class MyApp extends App {
     }
     // expose the page query to all the pages
     pageProps.query = ctx.query;
-    return { pageProps }
+    return { pageProps };
   }
 
   render() {
     const { Component, pageProps, apollo } = this.props;
     return (
       <ApolloProvider client={apollo}>
+        <p>Hello!</p>
+        
+        <Query
+          query={gql`
+            query LOCAL_CART_STATE_QUERY {
+              cartOpen @client
+            }
+          `}
+        >
+          {({ data }) => <p>Cart is {data.cartOpen ? "open" : "closed"}</p>}
+        </Query>
+
         {/* <Page> */}
-          <p>Hello!</p>
-          <Component {...pageProps} />
+        {/* <Component {...pageProps} /> */}
         {/* </Page> */}
       </ApolloProvider>
-    )
+    );
   }
 }
 
