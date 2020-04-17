085a6924d2866d493cc90faa1adb3e2f0ac337be
Kai Vandivier <kpvandivier@gmail.com>
2020-04-16 22:00:09 -0700

    Uncomment <Meta>

--- components/Page.js
+++ components/Page.js
@@ -24,7 +24,7 @@ const Page = props => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {/* <Meta /> */}
+      <Meta />
       {/* <Cart /> */}
       {/* <Header /> */}
       <Inner>
