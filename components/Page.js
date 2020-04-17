bf5a536d3289af8bbbae1f1d199273282a7ae8c6
Kai Vandivier <kpvandivier@gmail.com>
2020-04-16 21:52:31 -0700

    Comment out GQL parts

--- components/Page.js
+++ components/Page.js
@@ -1,10 +1,10 @@
 import React from "react";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import Cart from "./Cart";
 import Header from "./Header";
 import Meta from "./Meta";
 import theme from "./styles/theme";
 import GlobalStyles from "./styles/GlobalStyles";
-import Cart from "./Cart";
 
 const Inner = styled.main`
   padding: 1.5rem 2rem;
@@ -24,9 +24,9 @@ const Page = props => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Meta />
-      <Cart />
-      <Header />
+      {/* <Meta /> */}
+      {/* <Cart /> */}
+      {/* <Header /> */}
       <Inner>
         {props.children}
       </Inner>
