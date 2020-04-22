4ff608b2577170b9dfdcc40d4de2c04f542fb0b3
Kai Vandivier <kpvandivier@gmail.com>
2020-04-21 20:08:30 -0700

    Remove body parser

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // server.js
 const express = require('express');
-const bodyParser = require('body-parser');
 const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -11,20 +10,11 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express();
 
-  server.use(bodyParser.json());
-
   server.post("/", (req, res) => {
     res.status(405).end();
   })
 
   server.all("*", (req, res) => {
-    // if (req) {
-      // console.log(req.headers);
-      // console.log(res);
-    // }
-    console.log(req.headers);
-    console.log(req.body);
-    console.log(res.body)
     return handle(req, res);
   })
 
