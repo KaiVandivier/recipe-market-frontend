ca3733b53bf59c1556d289567caa2aa96a170c67
Kai Vandivier <kpvandivier@gmail.com>
2020-04-21 21:57:22 -0700

    Try rendering page

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import withData from "../lib/withData";
 import Page from "../components/Page";
 import { gql } from "apollo-boost"; //
 import { Query } from "@apollo/react-components"; //
-import { CURRENT_USER_QUERY } from "../components/User";
+import { CURRENT_USER_QUERY } from "../components/User"; //
 import "../public/nprogress.css";
 
 class MyApp extends App {
@@ -22,17 +22,17 @@ class MyApp extends App {
     const { Component, pageProps, apollo } = this.props;
     return (
       <ApolloProvider client={apollo}>
-        <p>Hello!</p>
+        {/* <p>Hello!</p>
 
         <Query
           query={CURRENT_USER_QUERY}
         >
           {({ loading }) => <p>Query is {loading ? "loading" : "done"}</p>}
-        </Query>
+        </Query> */}
 
-        {/* <Page> */}
-        {/* <Component {...pageProps} /> */}
-        {/* </Page> */}
+        <Page>
+          <Component {...pageProps} />
+        </Page>
       </ApolloProvider>
     );
   }
