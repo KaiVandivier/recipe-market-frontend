d9a8032d1afed1bd81bd0bfb4c50639e4d4e9284
Kai Vandivier <kpvandivier@gmail.com>
2020-05-22 22:24:01 -0700

    Add new icon

--- components/Meta.js
+++ components/Meta.js
@@ -1,14 +1,34 @@
 import Head from "next/head";
 
-import React from 'react';
+import React from "react";
 
 const Meta = () => {
   return (
     <Head>
       <meta charSet="utf-8" />
       {/* TODO: Viewport */}
-      {/* TODO: Icon */}
-      <link href='https://fonts.googleapis.com/css?family=Leckerli+One&display=swap' rel="stylesheet" /> 
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Leckerli+One&display=swap"
+        rel="stylesheet"
+      />
       <title>Recipe Market!</title>
     </Head>
   );
